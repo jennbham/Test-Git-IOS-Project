e9a14bf80e7b367cd71ca90fd7b70aa1b0d1a604
Jennifer Rinauro <jenrinauro@Jennifer-Rinauros-MacBook-Pro.local>
2013-11-01 21:15:11 -0700

    added a test class

---
+++ Git Test/JRTest.h
@@ -0,0 +1,13 @@
+//
+//  JRTest.h
+//  Git Test
+//
+//  Created by Jennifer Rinauro on 11/1/13.
+//  Copyright (c) 2013 Jennifer Rinauro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRTest : NSObject
+
+@end
---
+++ Git Test/JRTest.m
@@ -0,0 +1,13 @@
+//
+//  JRTest.m
+//  Git Test
+//
+//  Created by Jennifer Rinauro on 11/1/13.
+//  Copyright (c) 2013 Jennifer Rinauro. All rights reserved.
+//
+
+#import "JRTest.h"
+
+@implementation JRTest
+
+@end
